db0a34f0717861b3c454f5355bbc3c744e82785e
FlutLab.io <>
2021-02-18 21:41:39 +0000

    Autosave

--- lib/converter_screen.dart
+++ lib/converter_screen.dart
@@ -9,32 +9,48 @@ import 'unit.dart';
 class ConverterScreen extends StatelessWidget {
   /// Units for this [Category]
   final List<Unit> units;
+  final String title;
+  final Color color;
 
   /// This [ConverterScreen] requires the color and units to not be null.
   // TODO: Pass in the [Category]'s color
   const ConverterScreen({
     @required this.units,
-  }) : assert(units != null);
+    @requited this.title,
+    @required this.color,
+  })  : assert(units != null),
+        assert(title != null),
+        assert(color != null);
 
   @override
   Widget build(BuildContext context) {
     // Here is just a placeholder for a list of mock units
     final unitWidgets = units.map((Unit unit) {
       // TODO: Set the color for this Container
-      return Container(
-        margin: EdgeInsets.all(8.0),
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Text(
-              unit.name,
-              style: Theme.of(context).textTheme.headline,
-            ),
-            Text(
-              'Conversion: ${unit.conversion}',
-              style: Theme.of(context).textTheme.headline,
-            ),
-          ],
+      return Scaffold(
+        body: Container(
+          margin: EdgeInsets.all(8.0),
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                unit.name,
+                style: Theme.of(context).textTheme.headline,
+              ),
+              Text(
+                'Conversion: ${unit.conversion}',
+                style: Theme.of(context).textTheme.headline,
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: this.title,
+          backgroundColor: this.color,
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.arrow_back),
+          onPressed: _navigateToConverter(),
         ),
       );
     }).toList();
@@ -42,5 +58,10 @@ class ConverterScreen extends StatelessWidget {
     return ListView(
       children: unitWidgets,
     );
+  }
+
+  /// Navigates back to the [CategoryScreen].
+  void _navigateToConverter(BuildContext context) {
+    Navigator.pop(context);
   }
 }
