a4a401acd101cf3fa4945a18928ec73e0d19dd39
FlutLab.io <>
2021-02-18 21:04:24 +0000

    Autosave

--- lib/category_screen.dart
+++ lib/category_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'category.dart';
+import 'unit.dart';
 
 final _backGroundColor = Colors.green[100];
 
+/// Category Screen.
+///
+/// This is the 'home' screen of the Unit Converter. It shows a header and
+/// a list of [Categories].
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen();
 
@@ -11,6 +16,7 @@ class CategoryScreen extends StatelessWidget {
     'Area',
     'Volume',
     'Mass',
+    'Time',
     'Digital Storage',
     'Energy',
     'Currency',
@@ -27,6 +33,9 @@ class CategoryScreen extends StatelessWidget {
     Colors.red,
   ];
 
+  /// Makes the correct number of rows for the list view.
+  ///
+  /// For portrait, we use a [ListView].
   Widget _buildCategoryWidgets(List<Widget> categories) {
     return ListView.builder(
       itemCount: categories.length,
@@ -34,12 +43,28 @@ class CategoryScreen extends StatelessWidget {
     );
   }
 
+  /// Returns a list of mock [Unit]s.
+  List<Unit> _retrieveUnitList(String categoryName) {
+    return List.generate(10, (int i) {
+      i += 1;
+      return Unit(
+        name: '$categoryName Unit $i',
+        conversion: i.toDouble(),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final categories = <Category>[];
 
     for (var i = 0; i < _categoryNames.length; i++) {
-      categories.add(Category(_categoryNames[i], Icons.cake, _baseColors[i]));
+      categories.add(Category(
+        name: _categoryNames[i],
+        icon: Icons.cake,
+        color: _baseColors[i],
+        units: _retrieveUnitList(_categoryNames[i]),
+      ));
     }
 
     final listView = Container(
